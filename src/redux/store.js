74c6c11824b6bda3e9f596fc415b1789a88c9fae
VadimKorobec <_s_e_t__@ukr.net>
2023-02-19 11:38:05 +0200

    Update store.js

---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { persistStore, persistReducer } from 'redux-persist';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['contacts'],
+};
+
+export const store = configureStore();
