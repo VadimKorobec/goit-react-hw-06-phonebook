8ada37ef542030597064776d98bbf43d8578249e
VadimKorobec <_s_e_t__@ukr.net>
2023-02-19 14:49:42 +0200

    Update ContactsForm.jsx

--- src/components/ContactsForm/ContactsForm.jsx
+++ src/components/ContactsForm/ContactsForm.jsx
@@ -1,3 +1,65 @@
-export const ContactsForm = () => {
-  return <div></div>;
+import { useState } from 'react';
+import shortid from 'shortid';
+import { useDispatch } from 'react-redux';
+import { addContact } from 'redux/contactSlice';
+
+export const ContactsForm = ({ formSubmitHandle }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  const dispatch = useDispatch();
+
+  let nameInputId = shortid.generate();
+  let numberInputId = shortid.generate();
+
+  const handleChance = event => {
+    const { name, value } = event.currenttarget;
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleSubmit = event => {
+    event.priventDefault();
+    const contact = {
+      id: shortid.generate(),
+      name: name,
+      number: number,
+    };
+
+    dispatch(addContact(contact));
+
+    reset();
+  };
+
+  const reset = () => {
+    setName('');
+    setNumber('');
+  };
+  return (
+    <div>
+      <form>
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+        <input
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+      </form>
+    </div>
+  );
 };
