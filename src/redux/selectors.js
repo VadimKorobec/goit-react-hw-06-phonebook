88ae458c2ca23242aa13bb36a10dfe842f2e13aa
VadimKorobec <_s_e_t__@ukr.net>
2023-02-19 15:23:00 +0200

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -6,5 +6,10 @@ export const getFilter = state => state.filter;
 
 export const getFilteredConctacts = createSelector(
   [getContacts, getFilter],
-  (contacts, filter) => {}
+  (contacts, filter) => {
+    const normalizedFilter = filter.toLowerCase().trim();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  }
 );
