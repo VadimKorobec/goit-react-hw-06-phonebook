a4f2d2e12e777fe2924f11e5383d7ed48e73ae59
VadimKorobec <_s_e_t__@ukr.net>
2023-02-19 12:11:22 +0200

    Update filterSlice.js

---
+++ src/redux/filterSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const filterSlice = createSlice({
+  name: 'filter',
+  initialState: '',
+  reducers: {
+    setFilter(state, action) {},
+  },
+});
+
+export const filterReducer = filterSlice.reducer;
