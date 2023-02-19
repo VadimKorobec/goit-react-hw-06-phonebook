aff9dc7629dfd361a8d80e700e65aa8a232871d0
VadimKorobec <_s_e_t__@ukr.net>
2023-02-19 15:46:33 +0200

    Update ContactsListItem.jsx

--- src/components/ContactsListItem/ContactsListItem.jsx
+++ src/components/ContactsListItem/ContactsListItem.jsx
@@ -1,17 +1,19 @@
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
+import { deleteContact } from 'redux/contactSlice';
 
-export const ContactsListItem = ({ contact, deleteContact }) => {
+export const ContactsListItem = ({ contact }) => {
   const dispatch = useDispatch();
 
-  const deleteContact = () => {};
+  const handleDeleteContact = () => dispatch(deleteContact(contact.id));
+
   return (
     <div>
       <li>
         <span>
           {contact.name}:{contact.number}
         </span>
-        <button type="button" onClick={deleteContact}>
+        <button type="button" onClick={handleDeleteContact}>
           Delete
         </button>
       </li>
@@ -24,5 +26,4 @@ ContactsListItem.propTypes = {
     name: PropTypes.string.isRequired,
     number: PropTypes.string.isRequired,
   }).isRequired,
-  deleteContact: PropTypes.func.isRequired,
 };
