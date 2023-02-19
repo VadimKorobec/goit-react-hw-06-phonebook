7c6ac8c7f1303c9df43af365224464487b627c7e
VadimKorobec <_s_e_t__@ukr.net>
2023-02-19 19:00:21 +0200

    Update ContactsForm.jsx

--- src/components/ContactsForm/ContactsForm.jsx
+++ src/components/ContactsForm/ContactsForm.jsx
@@ -3,6 +3,7 @@ import shortid from 'shortid';
 import { useDispatch, useSelector } from 'react-redux';
 import { addContact } from 'redux/contactSlice';
 import { getContacts } from 'redux/selectors';
+import { Button, Form, Input, Label } from './ContactsForm.styled';
 
 export const ContactsForm = () => {
   const [name, setName] = useState('');
@@ -51,31 +52,35 @@ export const ContactsForm = () => {
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor={nameInputId}>Name</label>
-        <input
-          type="text"
-          name="name"
-          value={name}
-          onChange={handleChance}
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-          id={nameInputId}
-        />
-        <label htmlFor={numberInputId}>Number</label>
-        <input
-          type="tel"
-          name="number"
-          value={number}
-          onChange={handleChance}
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-          id={numberInputId}
-        />
-        <button type="submit">Add contact</button>
-      </form>
+      <Form onSubmit={handleSubmit}>
+        <Label htmlFor={nameInputId}>
+          Name
+          <Input
+            type="text"
+            name="name"
+            value={name}
+            onChange={handleChance}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            id={nameInputId}
+          />
+        </Label>
+        <Label htmlFor={numberInputId}>
+          Number
+          <Input
+            type="tel"
+            name="number"
+            value={number}
+            onChange={handleChance}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            id={numberInputId}
+          />
+        </Label>
+        <Button type="submit">Add contact</Button>
+      </Form>
     </>
   );
 };
