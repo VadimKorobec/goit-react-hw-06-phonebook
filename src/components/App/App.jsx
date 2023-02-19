83075f279b445221b79f54ef7e8950a6254d9d2c
VadimKorobec <_s_e_t__@ukr.net>
2023-02-19 14:49:44 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,49 @@
+import { ContactsForm } from 'components/ContactsForm/ContactsForm';
+import { ContactsList } from 'components/ContactsList/ContactsList';
+import { Filter } from 'components/Filter/Filter';
+import { useState, useEffect } from 'react';
+
 export const App = () => {
-  return <div></div>;
+  const startContacts =
+    JSON.parse(window.localStorage.getItem('contacts')) ?? [];
+  const [contacts, setContacts] = useState(startContacts);
+  const [filter, setFilter] = useState('');
+
+  const formSubmitHandler = contact => {
+    const notification = contacts.find(item => item.name === contact.name);
+
+    if (notification) {
+      alert(`${contact.name} is already in contacts`);
+    } else {
+      setContacts(prevState => [...prevState, contact]);
+    }
+  };
+
+  const filterContact = search => {
+    setFilter(search);
+  };
+
+  const deleteContact = contactId => {
+    setContacts(prevState =>
+      prevState.filter(contact => contact.id !== contactId)
+    );
+  };
+
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactsForm formSubmitHandle={formSubmitHandler} />
+      <h2>Contacts</h2>
+      <Filter filter={filter} filterContact={filterContact} />
+      <ContactsList
+        contacts={contacts}
+        filter={filter}
+        deleteContact={deleteContact}
+      />
+    </div>
+  );
 };
