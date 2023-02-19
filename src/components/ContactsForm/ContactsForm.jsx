ee904b2c84eaa9da212a49cddf550730dc17a3a2
VadimKorobec <_s_e_t__@ukr.net>
2023-02-19 16:03:38 +0200

    Update ContactsForm.jsx

--- src/components/ContactsForm/ContactsForm.jsx
+++ src/components/ContactsForm/ContactsForm.jsx
@@ -43,11 +43,13 @@ export const ContactsForm = ({ formSubmitHandle }) => {
     setNumber('');
   };
   return (
-    <div>
-      <form>
+    <>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           name="name"
+          value={name}
+          onChange ={handleChance}
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
@@ -55,11 +57,14 @@ export const ContactsForm = ({ formSubmitHandle }) => {
         <input
           type="tel"
           name="number"
+          value={name}
+          onChange={handleChance}
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
         />
+        <button type="submit">Add contact</button>
       </form>
-    </div>
+    </>
   );
 };
