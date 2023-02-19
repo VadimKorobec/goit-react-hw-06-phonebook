d2668a353c42e6f0e920821b3c8d5a4d5f3c5605
VadimKorobec <_s_e_t__@ukr.net>
2023-02-19 15:23:01 +0200

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,3 +1,19 @@
-export const Filter = () => {
-  return <div></div>;
+import PropTypes from 'prop-types';
+
+export const Filter = ({ filter, filterContact }) => {
+  const handleChange = event => {
+    const { value } = event.currentTarget;
+    filterContact(value);
+  };
+  return (
+    <div>
+      <input
+        type="text"
+        name="filter"
+        value={filter}
+        onChange={handleChange}
+        placeholder="Search..."
+      />
+    </div>
+  );
 };
